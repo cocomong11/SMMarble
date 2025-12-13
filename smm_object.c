5e12eb3efa40092ca9e8c59ce2f8253fc99f9b65
cocomong11 <carpediem1011@naver.com>
2025-12-13 17:54:27 +0900

    second

--- smm_object.c
+++ smm_object.c
@@ -33,55 +33,64 @@ static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = {
 };
 
 static int smm_nodeNr=0;
+//structure type definition
+#if 0
 static char smm_name[MAX_NODENR][MAX_CHARNAME];
 static int smm_type[MAX_NODENR];
 static int smm_credit[MAX_NODENR];
 static int smm_energy[MAX_NODENR];
+#endif
+
+typedef struct{
+	char name[MAX_CHARNAME];
+	int type;
+	int credit;
+	int energy;	
+} smmObj_board_t;
+
+static  smmObj_board_t smmObj_board[MAXNODENR];
+
+
+
 
 //object generation
 int smmObj_genNode(char* name, int type, int credit, int energy)
 {
-    strcpy(smm_name[smm_nodeNr],name);
-    smm_type[smm_nodeNr] = type;
-    smm_credit[smm_nodeNr] = credit;
-    smm_energy[smm_nodeNr] = energy;
+    strcpy(smmObj_board[smm_nodeNr].name,name);
+    smmObj_board[smm_nodeNr].type = type;
+    smmObj_board[smm_nodeNr]credit = credit;
+    smmObj_board[smm_nodeNr].energy = energy;
     
-    smm_nodeNr++;
+    smmObj_nodeNr++;
     
-    return (smm_nodeNr);
+    return (smmObj_nodeNr);
 }
 
 
 
 //member retrieving
-char* smmObj_getName(int node_nr)
+char* smmObj_getNodeName(int node_nr)
 {
-      return (smm_name[node_nr]);
+      return (smmObj_board[node_nr].name);
 }
 
-int smmObj_getType(int node_nr)
+int smmObj_getNodeType(int node_nr)
 {
-      return (smm_type[node_nr]);
+      return (smmObj_board[node_nr].type);
 }
 
-int smmObj_getEnergy(int node_nr)
+int smmObj_getNodeEnergy(int node_nr)
 {
-      return (smm_energy[node_nr]);
+      return (smmObj_board[node_nr].energy);
 }
 
-char* smmObj_getNodeName(int node_type)
+char* smmObj_getTypeName(int node_type)
 {
-      return (smmNodeName[node_type]);
+      return (smmObj_nodeName[node_type]);
 }
 
 
 #if 0
-//element to string
-char* smmObj_getNodeName(smmNode_e type)
-{
-    return smmNodeName[type];
-}
-
 char* smmObj_getGradeName(smmGrade_e grade)
 {
     return smmGradeName[grade];
